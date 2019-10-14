178bc0bd7a1a77de897e36326c156fe78b205b03
zmj0920 <mj960920@163.com>
2019-10-14 22:48:11 +0800

    vue自定义svg插件使用

---
+++ vue/vue-awesome/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <icon name="flag"></icon>
+    <faBell  class="svg-icon"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import faBell from "../assets/svg/fa-bell.svg";
+export default {
+  name: "home",
+  components: {
+    HelloWorld,
+    faBell
+  }
+};
+</script>
+<style lang="scss" scoped>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
