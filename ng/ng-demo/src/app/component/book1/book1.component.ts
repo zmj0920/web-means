fb404818ea6b4214ca6c2c8883c92cf4cf9660cb
GordonLz <451813289@qq.com>
2019-12-02 18:21:25 +0800

    update demo

--- ng/ng-demo/src/app/component/book1/book1.component.ts
+++ ng/ng-demo/src/app/component/book1/book1.component.ts
@@ -1,8 +1,14 @@
 import { addBook } from '@store/actions';
 import { AppStoreModule } from '@store/store.module';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
+import { Subscription, of} from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
+interface Lengthwise {
+  num: number;
+}
 @Component({
   selector: 'app-book1',
   templateUrl: './book1.component.html',
@@ -12,18 +18,48 @@ export class Book1Component implements OnInit {
   public bookName: string;
   public author: string;
   public price: number;
+  public name: Lengthwise;
   profileForm = this.fb.group({
-    bookName: [''],
-    author: [''],
-    price: [''],
+    bookName: ['', Validators.required],
+    author: ['', Validators.required],
+    price: ['', Validators.required],
     createAt: Date.now()
   });
+  private subscription: Subscription; // 订阅
   constructor(private fb: FormBuilder, private store: Store<AppStoreModule>) { }
 
-  ngOnInit() {
+ someValue = 123;
 
+ strLength: number = (this.someValue as unknown  as string).length;
+
+
+  loggingIdentity<T extends Lengthwise>(arg: T[]): T[] {
+    console.log(arg.length);
+    return arg;
   }
 
+  ngOnInit() {
+    // this.loggingIdentity({ length: 10, value: 3 });
+    this.subscription = of('桃子', '鲤鱼').pipe(
+      filter(v => v === '桃子'),
+      map(v => v + '罐头')
+    ).subscribe(console.log);
+
+
+    this.profileForm.valueChanges.pipe(
+      filter(() => this.profileForm.valid) ,
+      map(data => {
+        data.lastTime = new Date();
+        return data;
+      })
+    ).subscribe(res => console.log(res));
+  }
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnDestroy() {
+    // 取消订阅
+    this.subscription.unsubscribe();
+  }
   public submit(): void {
     if (this.bookName && this.author && this.price) {
       this.store.dispatch(addBook({ book: { bookName: this.bookName, author: this.author, price: this.price, createAt: Date.now() } }));
@@ -37,5 +73,11 @@ export class Book1Component implements OnInit {
     // TODO: Use EventEmitter with form value
     console.warn(this.profileForm.value);
     this.store.dispatch(addBook({ book: { ...this.profileForm.value } }));
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      bookName: 'Nancy'
+    });
   }
 }
--- ng/ng-demo/src/app/component/profile-editor/profile-editor.component.ts
+++ ng/ng-demo/src/app/component/profile-editor/profile-editor.component.ts
@@ -49,10 +49,3 @@ export class ProfileEditorComponent {
     console.warn(this.profileForm.value);
   }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
